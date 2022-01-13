7e19b45b18072170fb51ae83668a57ff9f875de4
jjackii <ruminor@naver.com>
2022-01-13 16:10:36 +0900

    Flask basic

---
+++ 01_flask_test/start.py
@@ -0,0 +1,5 @@
+import test
+
+print('start.py __name__:', __name__)
+
+print('start.py __name__:', test.add_one(10))  # module
---
+++ 01_flask_test/test.py
@@ -0,0 +1,11 @@
+def add_one(data):
+    return data + 1
+
+
+def add_two(data):
+    return data + 2
+
+
+if __name__ == '__main__':
+    print(add_one(10))
+    print(add_two(10))
